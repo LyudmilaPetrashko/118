a645c20e3a27ea92280ed455b52395b9d3dbe048
Mila <32988112+LyudmilaPetrashko@users.noreply.github.com>
2019-12-06 14:06:11 +0300

    Update 4 proga.c

--- 1 semester/4 proga.c
+++ 1 semester/4 proga.c
@@ -2,63 +2,11 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-void create_files (void)// нужно сздать файлы через цикл и проверить их на открытие
-{
-    //FILE *f1, *f2, *f3;
-    int  i = 0, j=1, n;
-    scanf("%d", &n);
-    int k=97;
-    char y[6];
-    y[1]='.';
-    y[2]='d';
-    y[3]='a';
-    y[4]='t';
-    y[5]='\0';
-    FILE* f;
-    for(i; i<n; i++){
-        y[0]=(k+i);
-        f=fopen( y , "w");
-        if(f!=NULL){
-            while (j < 6){
-            fprintf (f,"Record %d in file \"%s\"\n", j, y);
-                j++;
-            }
-         }
-        else printf("don't work");
-        fclose(f);
-    }
-    f = fopen ("a.dat","r");
-    print_file(f);
-    fclose(f);
-}
-
-
-void analyze_file (const char *file_name)
-{
-    char l;
-    int a = 0, Enter;
-    l = getc(file_name);
-    if(l==EOF)  Enter=0;
-    else{
-        Enter=1;
-        while (l != EOF)
-        {
-            if(l == 'a')
-                { a++; }
-            else if ( l == '\n')
-                { Enter++;}
-            printf("%c",l);
-            l = getc(file_name);
-        }
-    }
-    printf("\n a = %d\n", a);
-    printf(" /n = %d\n", Enter);
-}
-
 void print_file (FILE *f)
 {
     char a;
     a = fgetc(f);
+
     while (a != EOF) //NULL
     {
         if (a == 'a')
@@ -72,15 +20,74 @@ void print_file (FILE *f)
 }
 
 
+void create_files (void)// нужно сздать файлы через цикл и проверить их на открытие
+{
+    //FILE *f1, *f2, *f3;
+    int  i = 0, j=1, n;
+    scanf("%d", &n);
+    int k=97;
+    char y[] = "a.dat";
+    FILE* f;
+    for(i; i<n; i++){
+    //f1 = fopen ("a.dat","w");
+    //f2 = fopen ("b.dat","w");
+    //f3 = fopen ("c.dat","w");
+        j=1;
+        f=fopen( y , "w");
+        if(f!=NULL){
+            while (j < 6)
+            {
+                fprintf (f,"Record %d in file \"%s\"\n", j, y);
+          //fprintf (f2,"Record %d in file \"b.dat\"\n", j);
+          //fprintf (f3,"Record %d in file \"c.dat\"\n", j);
+                j++;
+            }
+            }
+            else printf("don't work");
+        fclose(f);
+        y [0]++;
+    }
+    //fclose (f1);
+    //fclose (f2);
+    //fclose (f3);
+}
+
+
+void analyze_file ( FILE *file_name)
+{
+    char l;
+    int a = 0, Enter=0;
+    l = getc(file_name);
+    while (l != EOF)
+    {
+        if(l == 'a')
+            { a++; }
+        else if ( l == '\n')
+            { Enter++;}
+       printf("%c",l);
+        l = getc(file_name);
+    }
+    printf("\n a = %d\n", a);
+    printf(" /n = %d\n", Enter);
+}
+
+
 int main()
 {
-
     FILE *f;
     f = fopen("file.txt", "r");
     create_files();
+    printf("\n");
     if(f==NULL) printf("don't work");
     else{
+    //char* z;
+    //float k;
+    //fscanf(f, "%s%f", z, &k);
     analyze_file(f);
+    printf("\n\n");
+    fclose (f);
+    f = fopen("file.txt", "r");
+    print_file(f);
     fclose (f);
     }
     return 0;

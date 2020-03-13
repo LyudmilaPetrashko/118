248e76b442b387ab21b518ecad77566853a23c68
Mila <32988112+LyudmilaPetrashko@users.noreply.github.com>
2020-03-13 15:53:05 +0300

    Create 1.3.cpp

---
+++ 2 semester/1.3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include<sstream>
+
+using namespace std;
+
+
+struct recs{
+    int k;
+    char str[256];
+} ;
+
+int main()
+{
+
+    int num;
+    cout<<"vvedite num"<< endl;
+    cin>>num;
+    cout<<endl;
+
+    recs *arr = new recs[num];
+
+    cin.getline(arr[0].str, 256);
+
+    int i = num;
+    while(i!=0){
+        cout<<"vvedite "<<num-i+1<<" stroky:"<<endl;
+        i--;
+        cin.getline(arr[i].str, 256);
+        //cin>>noskipws>>arr[i].str;
+        cout<<endl;
+    }
+
+    cout<<endl<<"array: "<<endl;
+    while(i!=num){
+        cout<<arr[i].str<<endl;
+        i++;
+    }
+
+    delete(arr);
+
+    return 0;
+}

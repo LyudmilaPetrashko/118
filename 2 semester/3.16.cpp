3703a0905e1dfefa28ab83cf5247b5e31c43652d
Mila <32988112+LyudmilaPetrashko@users.noreply.github.com>
2020-04-16 20:17:57 +0300

    Create 3.16.cpp

---
+++ 2 semester/3.16.cpp
@@ -0,0 +1,260 @@
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+ 
+class ok{
+    protected:
+   
+    double x, y, r;
+    
+    public:
+    
+    ok( double x1, double y1, double r1){
+        x=x1;
+        y=y1;
+        r=r1;
+    }
+    
+    ok(){
+        x=0;
+        y=0;
+        r=1;
+    }
+    
+    ok(const ok &other){
+        x=other.x;
+        y=other.y;
+        r=other.r;
+    }
+    
+    ok& operator = (const ok &st){
+        x=st.x;
+        y=st.y;
+        r=st.r;
+        return *this;
+    }
+    
+    double length() const{
+        return 2*M_PI*r;
+    }
+
+    ok& move(double dx, double dy){
+        x+=dx;
+        y+=dy;
+        return *this;
+    }
+    
+    
+    ok& change_xy(double x0, double y0){
+        x=x0;
+        y=y0;
+        return *this;
+    }
+    
+    ok& change_r(double r0){
+        r=r0;
+        return *this;
+    }
+    
+
+    void show() const{
+        cout<<endl<<"x="<<x<<"  y="<<y<<"  r="<<r<<endl<<endl;
+    }
+    
+    
+    ok& operator *=(double dz){
+        r*=dz;
+        return *this;
+    }
+    
+    ok operator *(double z) const{
+         ok l (*this);
+         l *= z;
+         return l;
+     }
+     
+    friend ok operator*(double a, const ok& m){
+        ok n=m;
+        return n*=a;
+    }
+    
+    bool operator==(const ok& oke ) const{
+	    return ((x==oke.x)&&(y==oke.y)&&(r==oke.r));
+    }
+    
+    bool operator!=(const ok& okey ) const{
+	    return  !((*this)==(okey));
+    }
+    
+    double get_x() const{
+        return x;
+    }
+    
+    double get_y() const{
+        return y;
+    }
+    
+    double get_r() const{
+        return r;
+    }
+    
+    
+    friend ostream &operator<<( ostream &output, const ok &k ){ 
+         output << "x=" << k.x << "  y=" << k.y<< "  r=" << k.r;
+         return output;            
+      }
+};
+
+class sf: public ok{
+     
+    double z;
+    
+    public:
+    
+    sf (double x1, double y1, double z1, double r1){
+        x=x1;
+        y=y1;
+        r=r1;
+        z=z1;
+    }
+    
+    sf(){
+        x=0;
+        y=0;
+        r=1;
+        z=0;
+        
+    }
+    
+    sf(const sf &other){
+        x=other.x;
+        y=other.y;
+        r=other.r;
+        z=other.z;
+    }
+    
+    sf& operator = (const sf &st){
+        x=st.x;
+        y=st.y;
+        r=st.r;
+        z=st.z;
+        return *this;
+    }
+    
+    double area() const{
+        return 4*M_PI*r*r;
+    }
+
+    sf& move_sf(double dx, double dy, double dz){
+        move(dx,dy);
+        z+=dz;
+        return *this;
+    }
+    
+    
+    sf& change_xyz(double x0, double y0, double z0){
+        change_xy(x0,y0);
+        z=z0;
+        return *this;
+    }
+    
+    sf& change_r(double r0){
+        r=r0;
+        return *this;
+    }
+    
+
+    void show() const{
+        cout<<endl<<"x="<<x<<"  y="<<y<<"  z="<<z<<"  r="<<r<<endl<<endl;
+    }
+    
+    
+    sf& operator *=(double dr){
+        r*=dr;
+        return *this;
+    }
+    
+    sf operator *(double t) const{
+         sf l (*this);
+         l *= t;
+         return l;
+     }
+     
+    friend sf operator*(double a, const sf& m){
+        sf n=m;
+        return n*=a;
+    }
+    
+    bool operator==(const sf& sfe ) const{
+	    return ((x==sfe.x)&&(y==sfe.y)&&(r==sfe.r)&&(z==sfe.z));
+    }
+    
+    bool operator!=(const sf& sfer ) const{
+	    return  !((*this)==(sfer));
+    }
+    
+    double get_x() const{
+        return x;
+    }
+    
+    double get_y() const{
+        return y;
+    }
+    
+    double get_z() const{
+        return z;
+    }
+    
+    double get_r() const{
+        return r;
+    }
+    
+    
+    friend ostream &operator<<( ostream &output, const sf &k ){ 
+         output << "x=" << k.x << "  y=" << k.y<<"  z="<<k.z<<"  r=" << k.r;
+         return output;            
+      }
+    
+    ~sf(){
+        cout<<endl<<"desructor";
+    }
+};
+
+int main(){
+    
+  sf s1, s2(2,2,2,3);
+  
+  cout<<"s1: "<<s1<<endl<<"s2: "<<s2<<endl;
+  
+  s1=s2;
+  cout<<endl<<"s1(=): "<<s1<<endl;
+  
+  cout<<endl<<"area="<<s1.area()<<endl;
+  
+  cout<<endl<<"s1(move): "<<s1.move_sf(1,1,1)<<endl;
+  
+  cout<<endl<<"s1(ch_xyz): "<<s1.change_xyz(2,2,2)<<endl;
+  
+  cout<<endl<<"s1(ch_r): "<<s1.change_r(2)<<endl;
+  
+  cout<<endl<<"s1(get): "<<"x="<<s1.get_x()<<"  y="<<s1.get_y()<<"  z="<<s1.get_z()<<"  r="<<s1.get_r();
+  
+  s1*=5;
+  cout<<endl<<"s1(*=): "<<s1<<endl;
+  
+  s1=s1*0.2;
+  cout<<endl<<"s1(*): "<<s1<<endl;
+  
+  bool f;
+  f=(s1==s2);
+  cout<<endl<<"s1==s2?: "<<f<<endl;
+  
+  f=(s1!=s2);
+  cout<<endl<<"s1!=s2?: "<<f<<endl;
+  
+  cout<<endl;
+  return 0;
+}

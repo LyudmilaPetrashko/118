b08e8edc8b938489111d8d6e482c82f32c737f63
Mila <32988112+LyudmilaPetrashko@users.noreply.github.com>
2020-05-25 13:48:09 +0300

    Update 6.16.cpp

--- 2 semester/6.16.cpp
+++ 2 semester/6.16.cpp
@@ -18,12 +18,12 @@ void zadacha_koshi(double a, double b, double y0, double dx, double l){
     double x=a;
     double k1, k2, k3, k4;
 
-    ofstream fout("cppstudio.txt");
-
-    if(fout==NULL) cout<<"don't work";
+    ofstream ofile;
+    ofile.open ("cppstudio.txt", ios_base::out);
+    if(ofile==NULL) cout<<"don't work";
 
     else{
-        fout<<"x="<<x<<"\t"<<"    y="<<y_i<<"\t"<<"   y_an="<<l*x*x/2+y0<<"\t"<<endl;
+        ofile<<"x="<<x<<"\t"<<"    y="<<y_i<<"\t"<<"   y_an="<<l*x*x/2+y0<<"\t"<<endl;
 
         for(int i=0; i<int(n); i++){
 
@@ -35,10 +35,10 @@ void zadacha_koshi(double a, double b, double y0, double dx, double l){
             y_i+=dx/6*(k1+2*k2+2*k3+k4);
             x+=dx;
 
-            fout<<"x="<<x<<"\t"<<"    y="<<y_i<<"\t"<<"   y_an="<<l*x*x/2.+y0<<"\t"<<endl;
+            ofile<<"x="<<x<<"\t"<<"    y="<<y_i<<"\t"<<"   y_an="<<l*x*x/2.+y0<<"\t"<<endl;
         }
 
-    fout.close();
+    ofile.close();
     }
 }
 
